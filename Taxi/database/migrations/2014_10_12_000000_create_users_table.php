59684bd5fc356e4301ac79bf8a0cb61fd62e4805
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-02-25 22:57:15 +0000

    dele: Supprimer une column de la migration 'users'

--- Taxi/database/migrations/2014_10_12_000000_create_users_table.php
+++ Taxi/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
-            $table->string('prenom');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone');
