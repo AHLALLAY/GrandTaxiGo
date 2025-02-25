6b097a2a124873267b2091b5ffc046a41351e9f5
abderrahmane ahlalay <abderrahmanahlalay76@gmail.com>
2025-02-25 22:20:36 +0000

    feat: Ajouter les migrations 'driver', 'route', et 'reservation'

---
+++ Taxi/database/migrations/2025_02_25_172800_create_reservation__table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservation', function (Blueprint $table) {
+            $table->id();
+            $table->integer('passenger');
+            $table->integer('driver');
+            $table->integer('route');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservation_');
+    }
+};
